0c62c4ac31037d96629ba388106130beb48943b9
nmanzanares <51924693+nmanzanares@users.noreply.github.com>
2026-05-12 18:10:08 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flashcards-v15'; // Cambia el nombre cada vez que edites la app
+const CACHE_NAME = 'flashcards-v16'; // Cambia el nombre cada vez que edites la app
 const ASSETS = [
     './',
     './index.html',
